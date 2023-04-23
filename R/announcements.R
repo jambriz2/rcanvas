b9ee09b64884e2aeb982d3a57a20f8b61099acda
jambriz2 <ambrizjesus74@gmail.com>
2023-04-23 11:43:08 -0700

    Added Fix announcements f()

--- R/announcements.R
+++ R/announcements.R
@@ -34,25 +34,35 @@
 #' Get announcements from a Canvas course
 #'
 #' @param course_id numeric course id
-# @param start_date Only return announcements posted since the start_date (inclusive). Defaults to 14 days ago. The value should be formatted as: yyyy-mm-dd or ISO 8601 YYYY-MM-DDTHH:MM:SSZ.
-# @param end_date Only return announcements posted before the end_date (inclusive). Defaults to 28 days from start_date. The value should be formatted as: yyyy-mm-dd or ISO 8601 YYYY-MM-DDTHH:MM:SSZ. Announcements scheduled for future posting will only be returned to course administrators.
+#' @param start_date Only return announcements posted since the start_date (inclusive). Defaults to 14 days ago. The value should be formatted as: yyyy-mm-dd or ISO 8601 YYYY-MM-DDTHH:MM:SSZ.
+#' @param end_date Only return announcements posted before the end_date (inclusive). Defaults to 28 days from start_date. The value should be formatted as: yyyy-mm-dd or ISO 8601 YYYY-MM-DDTHH:MM:SSZ. Announcements scheduled for future posting will only be returned to course administrators.
 # @param active_only	Only return active announcements that have been published. Applies only to requesting users that have permission to view unpublished items. Defaults to false for users with access to view unpublished items, otherwise true and unmodifiable.
 #
 #' @return announcements for a given course
 #' @export
-get_announcements <- function(course_id) {
+#
+#' @examples
+#' \dontrun{get_announcements(course_id = 27)}
+#' \dontrun{get_announcements(course_id = 27, start_date = "2017-02-01")}
+get_announcements <- function(course_id, start_date = NULL, end_date = NULL) {
   # Define the endpoint for the announcements
-  endpoint <- make_canvas_url("courses", course_id, "discussion_topics")
+  url <- paste0(canvas_url(), "announcements")
 
   # Set the required parameters for the API call
-  args <- list(only_announcements = "1", per_page = "100")
+  context_code <- paste0("course_", course_id)
+  arg <- list(per_page = 100)
+  include <- iter_args_list(context_codes, "context_codes[]")
+  include2 <- iter_args_list(start_date, "start_date")
+  include3 <- iter_args_list(end_date, "end_date")
+  args <- c(arg, include, include2, include3)
+ # args <- list(context_code = "context_codes[]", start_date = "start_date", end_date = "end_date")
 
   # Make the API call
   response <- canvas_query(endpoint, args)
 
   # Process the response and convert it to a data frame
-  announcements <- httr::content(response, "parsed")
-  announcements_df <- as.data.frame(announcements)
+  announcements_df <- process_response(url, args)
+  announcements_df
 
   # Return the announcements data frame
   return(announcements_df)
